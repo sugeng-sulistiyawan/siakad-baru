a0da733019db7448aeb1f6179afa3232e684eb32
Sugeng Sulistiyawan <sugeng.sulistiyawan@gmail.com>
2020-09-10 21:03:27 +0700

    update app name

--- config/web.php
+++ config/web.php
@@ -7,6 +7,7 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
+    'name' => APP_NAME,
     'basePath' => BASE_PATH,
     'vendorPath' => VENDOR_PATH,
     'bootstrap' => ['log'],
